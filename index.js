4667b81ca33f67baf67a06c0b35a45e77cf96aa1
Bhavya Lakhani <bhavyalakhani89@gmail.com>
2024-12-08 14:13:03 -0500

    changes for assignment 6

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Lab5 from './Lab5/index.js'
 import UserRoutes from './Kanbas/Users/routes.js'
 import cors from "cors";
 import session from "express-session";
+import mongoose from 'mongoose';
 import "dotenv/config";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
@@ -11,6 +12,11 @@ import EnrollmentsRoutes from './Kanbas/Enrollments/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 
 const app = express();
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
+
 app.use(
     cors({
       credentials: true,
